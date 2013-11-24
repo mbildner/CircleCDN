e294a0988a39a5d26646cf2221e21f3ec3461c3e
Moshe Bildner <moshe.bildner@gmail.com>
2013-11-24 13:51:52 -0500

    Swapping in Tornado instead of Gevent.

---
+++ tornado_router.py
@@ -0,0 +1,96 @@
+import json
+from random import choice
+
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import tornado.options
+from tornado import template
+
+
+
+# -------------------------
+# temporary code 
+templates_path = "./"
+
+loader = template.Loader(templates_path)
+
+
+
+
+
+# -------------------------
+
+
+
+def get_random_id(length):
+	rand_char_string = "0123456789abcdefghijklmnopqrstuvwxyz"
+	return "".join([choice(rand_char_string) for x in range(length)])
+
+
+
+class Dispatcher(object):
+	def __init__(self):
+		self.websockets = {}
+
+	def register_websocket(self, userid, websocket):
+		self.websockets[userid] = websocket
+
+	def unregister_websocket(self, userid):
+		del self.websockets[userid]
+
+	def route_message(self, message):
+		self.websockets[json.loads(message)['RecipientID']].write_message(message)
+
+
+
+dispatcher = Dispatcher()
+
+
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+	def open(self):
+		self.userid = self.get_cookie('userid')
+		dispatcher.register_websocket(self.userid, self)
+
+	def on_close(self):
+		print 'connection closed'
+
+	def on_message(self, message):
+		dispatcher.route_message(message)
+
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.userid = self.get_cookie('userid')
+		if not self.userid:
+			self.userid = get_random_id(10)
+			self.set_cookie('userid', self.userid)
+
+		rendered_template = loader.load("tornado_template.html").generate(userid=self.userid)
+
+		self.write(rendered_template)
+
+
+class RTCBounceHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.userid = self.get_cookie('userid')
+
+	
+
+
+application = tornado.web.Application([
+    (r'/signalsocket', WSHandler),
+    (r'/', MainHandler)
+])
+ 
+if __name__ == "__main__":
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8000)
+
+    print "Tornado server starting."
+    tornado.ioloop.IOLoop.instance().start()
+
+
+ 
