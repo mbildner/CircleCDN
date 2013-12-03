15db7e896a6f0cd472a8d760d6d4854291f7fca3
Moshe Bildner <moshe.bildner@gmail.com>
2013-12-03 14:33:08 -0500

    Beginning work on mini tcp wrapper for datachannel chunking and reliability.

--- static/javascript/tcp.js
+++ static/javascript/tcp.js
@@ -1,24 +1,33 @@
-var data = JSON.stringify([1,2,3,4,5,6,7])
-
-var chunked = function (data, pieces) {
-	var chunkedData = [];
-	var index = 0;
-	var chunkSize = Math.ceil(data.length / pieces);
-	for (var i=0; i < pieces; i++) {
-		console.log(chunkSize);
-		var chunk = data.slice(i*chunkSize, (i+1) * chunkSize);
-		chunkedData.push(chunk);
-	}
-	return chunkedData;
-}
-
-
-
 var ReliableSender = function (channel) {
+
+	var chunkMessage = function (data, pieces) {
+		var chunkedData = [];
+		var index = 0;
+		var chunkSize = Math.ceil(data.length / pieces);
+		for (var i=0; i < pieces; i++) {
+			var chunk = data.slice(i*chunkSize, (i+1) * chunkSize);
+			chunkedData.push(chunk);
+		}
+		return chunkedData;
+	};
+
+
+
 	var send = function (message) {
+		var chunked = chunkMessage(message, 5);
+		console.log(chunked);
+	};
 
-	}
+
+	var processMessage = function (message) {
+		console.log(message);
+	};
 
 
+
+
+	this.send = send;
+
+	return this;
 
 }
